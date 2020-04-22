6778a0027fe53f2758a7c041ab0ad1fa8faf56c0
Dr. ERDI Gergo <gergo@erdi.hu>
2020-04-22 19:28:21 +0800

    Add hand-written Barbies

--- src/Hello.hs
+++ src/Hello.hs
@@ -8,12 +8,32 @@ import Barbies.Bare
 import Data.Barbie.TH
 
 declareBareB [d|
-  data MyBarbie = MyBarbie
+  data MyBarbieTH = MyBarbieTH
       { bar :: "BAR" ::: Bool
       , baz :: "BAZ" ::: Int
       } |]
 
-topEntity :: "FOO" ::: MyBarbie Covered (Signal System)
-topEntity = MyBarbie (pure True) (pure 0)
+-- This one works
+data MyBarbieManual1 c f = MyBarbieManual1
+    { bar' :: "BAR" ::: Wear c f Bool
+    , baz' :: "BAZ" ::: Wear c f Int
+    }
+
+-- This is what `MyBarbieTH` is translated into...
+data MyBarbieManual2 c f = MyBarbieManual2
+    { bar2 :: Wear c f ("BAR" ::: Bool)
+    , baz2 :: Wear c f ("BAZ" ::: Int)
+    }
+
+topEntity
+    :: ( "FOO" ::: MyBarbieTH Covered (Signal System)
+       , "QUUX" ::: MyBarbieManual1 Covered (Signal System)
+       , "FLOB" ::: MyBarbieManual2 Covered (Signal System)
+       )
+topEntity =
+    ( MyBarbieTH (pure True) (pure 0)
+    , MyBarbieManual1 (pure True) (pure 0)
+    , MyBarbieManual2 (pure True) (pure 0)
+    )
 
 makeTopEntity 'topEntity
